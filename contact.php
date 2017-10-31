04f2fea11cd63a2fe4677fab47bd26f985904f9e
smahnke <mahnkes1@msu.edu>
2017-10-31 13:47:16 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,17 +4,17 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'Immigrant Imprints <filammich@fil-am-michigan.matrix.msu.edu>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'mahnkes1@msu.edu';
 
 // subject of the email
-$subject = 'New message from contact form';
+$subject = 'New message from Immigrant Imprints contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
+$fields = array('name' => 'Name','email' => 'Email', 'phone' => 'Phone','message' => 'Message'); 
 
 // message that will be displayed when everything is OK :)
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
@@ -73,3 +73,4 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
 else {
     echo $responseArray['message'];
 }
+?>
